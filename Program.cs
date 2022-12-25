3dcbb5a6ed50e29fcd410cb2788600f3ebecdb2b
Gavryushkina022 <YANKOVAINNA@gmail.com>
2022-12-25 19:27:54 +0300

    Add programme code

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿
-![]FinalProject1;
+string[] array = new string[] { "hello", "2", "world", ":-)", "123", "78" };
 
+string[] FilterArray(string[] array)
+{
+    string str = "";
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+        {
+            str += i != (array.Length - 1) ? array[i] + "," : array[i];
+        }
+        i++;
+    }
+    return str.Split(",");
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(i != (array.Length - 1) ? array[i] + "," : array[i]);
+    }
+    Console.Write("]");
+}
+
+PrintArray(FilterArray(array));
